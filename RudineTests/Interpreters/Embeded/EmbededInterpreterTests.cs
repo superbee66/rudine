e1417b5c9fab0f2a107ca195de06058109253d5e
g@progablab.com <g@progablab.com>
2017-04-13 12:23:01 -0400

    nunit 3x test adapter added to solution, first test written

---
+++ RudineTests/Interpreters/Embeded/EmbededInterpreterTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Rudine.Interpreters.Embeded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rudine.Web;
+
+namespace Rudine.Interpreters.Embeded.Tests
+{
+    [TestFixture()]
+    public class EmbededInterpreterTests
+    {
+        [Test()]
+        public void CreateTest()
+        {
+            var basedoc = new EmbededInterpreter().Create(string.Empty);
+            Assert.IsTrue(basedoc is BaseDoc);
+        }
+    }
+}
