52d023ea6abd3d5ad42a763951b00f56ca1a049d
G <g@progablab.com>
2017-04-20 16:35:14 -0700

    removed expressions in favor for method blocks so the code could folder in the IDE

--- Rudine/template/Docdb/DocdbTemplateController.cs
+++ Rudine/template/Docdb/DocdbTemplateController.cs
@@ -31,65 +31,69 @@ namespace Rudine.Template.Docdb
                        : null;
         }
 
-        public string TopDocRev(string DocTypeName) =>
-            DocExchange.LuceneController.List(
-                           new List<string> { EmbededTemplateController.MY_ONLY_DOC_NAME },
-                           new Dictionary<string, List<string>>
-                           {
-                               {
-                                   "TargetDocTypeName", new List<string> { DocTypeName }
-                               }
-                           })
-                       .Select(_LightDoc => _LightDoc.GetTargetDocVer())
-                       .OrderByDescending(s => new Version(s))
-                       .FirstOrDefault();
+        public string TopDocRev(string DocTypeName)
+        {
+            return DocExchange.LuceneController.List(
+                                  new List<string> { EmbededTemplateController.MY_ONLY_DOC_NAME },
+                                  new Dictionary<string, List<string>>
+                                  {
+                                      {
+                                          "TargetDocTypeName", new List<string> { DocTypeName }
+                                      }
+                                  })
+                              .Select(_LightDoc => _LightDoc.GetTargetDocVer())
+                              .OrderByDescending(s => new Version(s))
+                              .FirstOrDefault();
+        }
 
-        private static byte[] GetCabDecodedAttachment(string DocTypeName, string DocTypeVer) =>
-            CacheMan.Cache(() =>
-                           {
-                               Dictionary<string, string> DocKeys;
+        private static byte[] GetCabDecodedAttachment(string DocTypeName, string DocTypeVer)
+        {
+            return CacheMan.Cache(() =>
+                                  {
+                                      Dictionary<string, string> DocKeys;
 
-                               const int SP1Header_Size = 20;
-                               const int FIXED_HEADER = 16;
+                                      const int SP1Header_Size = 20;
+                                      const int FIXED_HEADER = 16;
 
-                               string attachmentName;
-                               byte[] reaponseFile = null;
+                                      string attachmentName;
+                                      byte[] reaponseFile = null;
 
-                               string DocSrc;
+                                      string DocSrc;
 
-                               object o = DocExchange.LuceneController.Get(
-                                   EmbededTemplateController.MY_ONLY_DOC_NAME,
-                                   new Dictionary<string, string> { { "TargetDocTypeVer", DocTypeVer }, { "TargetDocTypeName", DocTypeName } });
+                                      object o = DocExchange.LuceneController.Get(
+                                          EmbededTemplateController.MY_ONLY_DOC_NAME,
+                                          new Dictionary<string, string> { { "TargetDocTypeVer", DocTypeVer }, { "TargetDocTypeName", DocTypeName } });
 
-                               if (o == null)
-                                   o = DocExchange.LuceneController.Get(
-                                       EmbededTemplateController.MY_ONLY_DOC_NAME,
-                                       new Dictionary<string, string> { { "DocTypeVer", DocTypeVer }, { "DocTypeName", DocTypeName } });
+                                      if (o == null)
+                                          o = DocExchange.LuceneController.Get(
+                                              EmbededTemplateController.MY_ONLY_DOC_NAME,
+                                              new Dictionary<string, string> { { "DocTypeVer", DocTypeVer }, { "DocTypeName", DocTypeName } });
 
-                               byte[] decodedAttachment = null;
-                               if (o != null)
-                               {
-                                   IDocRev _IDocRev = (IDocRev) o;
+                                      byte[] decodedAttachment = null;
+                                      if (o != null)
+                                      {
+                                          IDocRev _IDocRev = (IDocRev) o;
 
-                                   using (MemoryStream ms = new MemoryStream(_IDocRev.TargetDocTypeFiles))
-                                   using (BinaryReader theReader = new BinaryReader(ms))
-                                   {
-                                       //Position the reader to get the file size.
-                                       byte[] headerData = new byte[FIXED_HEADER];
-                                       headerData = theReader.ReadBytes(headerData.Length);
+                                          using (MemoryStream ms = new MemoryStream(_IDocRev.TargetDocTypeFiles))
+                                          using (BinaryReader theReader = new BinaryReader(ms))
+                                          {
+                                              //Position the reader to get the file size.
+                                              byte[] headerData = new byte[FIXED_HEADER];
+                                              headerData = theReader.ReadBytes(headerData.Length);
 
-                                       int fileSize = (int) theReader.ReadUInt32();
-                                       int attachmentNameLength = (int) theReader.ReadUInt32() * 2;
+                                              int fileSize = (int) theReader.ReadUInt32();
+                                              int attachmentNameLength = (int) theReader.ReadUInt32() * 2;
 
-                                       byte[] fileNameBytes = theReader.ReadBytes(attachmentNameLength);
-                                       //InfoPath uses UTF8 encoding.
-                                       Encoding enc = Encoding.Unicode;
+                                              byte[] fileNameBytes = theReader.ReadBytes(attachmentNameLength);
+                                              //InfoPath uses UTF8 encoding.
+                                              Encoding enc = Encoding.Unicode;
 
-                                       //attachmentName = enc.GetString(fileNameBytes, 0, attachmentNameLength - 2);
-                                       decodedAttachment = theReader.ReadBytes(fileSize);
-                                   }
-                               }
-                               return decodedAttachment;
-                           }, false, "GetCabDecodedAttachment", DocTypeName, DocTypeVer);
+                                              //attachmentName = enc.GetString(fileNameBytes, 0, attachmentNameLength - 2);
+                                              decodedAttachment = theReader.ReadBytes(fileSize);
+                                          }
+                                      }
+                                      return decodedAttachment;
+                                  }, false, "GetCabDecodedAttachment", DocTypeName, DocTypeVer);
+        }
     }
 }
