79517bbfd5283ab77e79061a42245f9fcb075fac
g@progablab.com <g@progablab.com>
2017-06-15 23:20:40 -0700

    located byte interpreter as well as text ones to try creating a template with

--- Rudine/Interpreters/DocInterpreter.cs
+++ Rudine/Interpreters/DocInterpreter.cs
@@ -13,14 +13,12 @@ using Rudine.Util.Zips;
 using Rudine.Web;
 using Rudine.Web.Util;
 
-namespace Rudine.Interpreters
-{
+namespace Rudine.Interpreters {
     /// <summary>
     ///     the master head of an abstract factory pattern applied to bridge our CLR objects & values to native document
     ///     formats supported
     /// </summary>
-    internal class DocInterpreter : IDocTextInterpreter, IDocByteInterpreter
-    {
+    internal class DocInterpreter : IDocTextInterpreter, IDocByteInterpreter {
         internal static readonly DocBaseInterpreter[] ContentInterpreterInstances =
             Reflection
                 .LoadBinDlls()
@@ -28,7 +26,7 @@ namespace Rudine.Interpreters
                 .Where(type => !type.IsAbstract)
                 .Where(type => !type.IsInterface)
                 .Where(type => type.BaseType == typeof(DocTextInterpreter) || type.BaseType == typeof(DocByteInterpreter))
-                .Select(type => ((DocBaseInterpreter)Activator.CreateInstance(type)))
+                .Select(type => ((DocBaseInterpreter) Activator.CreateInstance(type)))
                 .ToArray();
 
         public static readonly DocInterpreter Instance = new DocInterpreter();
@@ -63,8 +61,7 @@ namespace Rudine.Interpreters
         /// </summary>
         /// <param name="baseDoc"></param>
         /// <returns></returns>
-        public int CalcDocChecksum(BaseDoc baseDoc, bool? docStatus = null)
-        {
+        public int CalcDocChecksum(BaseDoc baseDoc, bool? docStatus = null) {
             docStatus = docStatus ?? baseDoc.DocStatus;
 
             // absolutely necessary the object is not altered in any way shape of form
@@ -77,8 +74,7 @@ namespace Rudine.Interpreters
                 p.SetValue(baseDoc, Activator.CreateInstance(p.PropertyType), null);
 
             using (StringWriter _StringWriter = new StringWriter())
-            using (XmlTextWriter _XmlTextWriter = new XmlTextWriter(_StringWriter))
-            {
+            using (XmlTextWriter _XmlTextWriter = new XmlTextWriter(_StringWriter)) {
                 new XmlSerializer(baseDoc.GetType()).Serialize(_XmlTextWriter, baseDoc);
                 return _StringWriter.ToString()
                                     .GetHashCode() ^ (docStatus ?? false).GetHashCode();
@@ -91,13 +87,25 @@ namespace Rudine.Interpreters
                     TemplateController.Instance.TopDocRev(DocTypeName))
                 .Create(DocTypeName);
 
-        public DocRev CreateTemplate(List<DocRevEntry> docFiles, string docTypeName = null, string docRev = null, string schemaXml = null, List<CompositeProperty> schemaFields = null)
-        {
+        /// <summary>
+        ///     locates a ContentInterpreter based on the claimed TemplateSource ContentFileExtension that matches one of the
+        ///     docFiles passed. continues probing and testing until someone can produce a DocRev
+        /// </summary>
+        /// <param name="docFiles"></param>
+        /// <param name="docTypeName"></param>
+        /// <param name="docRev"></param>
+        /// <param name="schemaXml"></param>
+        /// <param name="schemaFields"></param>
+        /// <returns></returns>
+        public DocRev CreateTemplate(List<DocRevEntry> docFiles, string docTypeName = null, string docRev = null, string schemaXml = null, List<CompositeProperty> schemaFields = null) {
             DocRev _DocRev = null;
 
-            foreach (string Extension in docFiles.Select(_ => new FileInfo(_.Name).Extension.Trim('.')).Distinct())
-                foreach (DocTextInterpreter _IDocDataInterpreter in ContentInterpreterInstances.OfType<DocTextInterpreter>())
-                    if (_IDocDataInterpreter.TemplateSources().Any(templateSource => templateSource.ContentFileExtension.Trim('.').Equals(Extension, StringComparison.CurrentCultureIgnoreCase)))
+            foreach (string Extension in docFiles.Select(_ => new FileInfo(_.Name).Extension.Trim('.'))
+                                                 .Distinct())
+                foreach (DocBaseInterpreter _IDocDataInterpreter in ContentInterpreterInstances)
+                    if (_IDocDataInterpreter.TemplateSources()
+                                            .Any(templateSource => templateSource.ContentFileExtension.Trim('.')
+                                                                                 .Equals(Extension, StringComparison.CurrentCultureIgnoreCase)))
                         if (_DocRev != null)
                             break;
                         else
@@ -116,8 +124,7 @@ namespace Rudine.Interpreters
                 .HrefVirtualFilename(DocTypeName, DocRev);
 
         internal static T InstanceLocatorByName<T>(string DocTypeName, string DocRev = null) where T : DocBaseInterpreter =>
-            CacheMan.Cache(() =>
-                           {
+            CacheMan.Cache(() => {
                                foreach (T _IDocDataInterpreter in ContentInterpreterInstances.OfType<T>())
                                    //TODO:Need a better way of discovering what IDocDataInterpreter can process the given document; it needs to consider the DocRev also
                                    if (_IDocDataInterpreter.Processable(DocTypeName, DocRev))
@@ -125,24 +132,21 @@ namespace Rudine.Interpreters
                                throw new Exception(String.Format("{0} {1}, {2} could not locate a DocDataInterpreter to process the data passed", DocTypeName, DocRev, typeof(DocInterpreter).Name));
                            }, false, "InstanceLocatorByName", DocTypeName, DocRev ?? String.Empty);
 
-        private static DocTextInterpreter LocateInstance(string DocData)
-        {
+        private static DocTextInterpreter LocateInstance(string DocData) {
             foreach (DocTextInterpreter _IDocDataInterpreter in ContentInterpreterInstances.OfType<DocTextInterpreter>())
                 if (!String.IsNullOrWhiteSpace(_IDocDataInterpreter.ReadDocTypeName(DocData)) && !String.IsNullOrWhiteSpace(_IDocDataInterpreter.ReadDocRev(DocData)))
                     return _IDocDataInterpreter;
             throw new InterpreterLocationException();
         }
 
-        private static DocByteInterpreter LocateInstance(byte[] DocData)
-        {
+        private static DocByteInterpreter LocateInstance(byte[] DocData) {
             foreach (DocByteInterpreter _IDocDataInterpreter in ContentInterpreterInstances.OfType<DocByteInterpreter>())
                 if (!String.IsNullOrWhiteSpace(_IDocDataInterpreter.ReadDocTypeName(DocData)) && !String.IsNullOrWhiteSpace(_IDocDataInterpreter.ReadDocRev(DocData)))
                     return _IDocDataInterpreter;
             throw new InterpreterLocationException();
         }
 
-        private static DocProcessingInstructions MergePI(bool? DocStatus, Dictionary<string, string> DocKeys, string DocTitle, DocProcessingInstructions _DocProcessingInstructions, int? DocChecksum = null, string href = null)
-        {
+        private static DocProcessingInstructions MergePI(bool? DocStatus, Dictionary<string, string> DocKeys, string DocTitle, DocProcessingInstructions _DocProcessingInstructions, int? DocChecksum = null, string href = null) {
             _DocProcessingInstructions.DocStatus = DocStatus ?? _DocProcessingInstructions.DocStatus;
             _DocProcessingInstructions.DocTitle = DocTitle ?? _DocProcessingInstructions.DocTitle;
             _DocProcessingInstructions.DocKeys = DocKeys ?? _DocProcessingInstructions.DocKeys;
@@ -178,8 +182,7 @@ namespace Rudine.Interpreters
             InstanceLocatorByName<DocBaseInterpreter>(DocTypeName, DocRev)
                 .Processable(DocTypeName, DocRev);
 
-        public virtual void ProcessRequest(HttpContext context)
-        {
+        public virtual void ProcessRequest(HttpContext context) {
             // ensure the latest content has been processed & imported
             ImporterController.CreateTemplateItems(DocExchange.Instance);
 
@@ -189,14 +192,12 @@ namespace Rudine.Interpreters
                 &&
                 !string.IsNullOrWhiteSpace(_TemplateFileInfo.solutionVersion)
                 &&
-                !string.IsNullOrWhiteSpace(_TemplateFileInfo.FileName))
-            {
+                !string.IsNullOrWhiteSpace(_TemplateFileInfo.FileName)) {
                 // serve a supporting template file up (the the target Interpreter may need to alter it in some way, qazi-dynamic)
                 InstanceLocatorByName<DocBaseInterpreter>(_TemplateFileInfo.DocTypeName, _TemplateFileInfo.solutionVersion)
                     .ProcessRequest(context);
             }
-            else
-            {
+            else {
                 // serve a filled & stored document rendered previously
                 context.Response.DisableKernelCache();
                 context.Response.Clear();
@@ -207,12 +208,12 @@ namespace Rudine.Interpreters
 
                 if (context.Request.Params.AllKeys.Any(m => m == Parm.DocId.ToString()))
                     docData = DocExchange.LuceneController.GetDocData(
-                        context.Request.Params[Parm.DocTypeName],
-                        context.Request.Params[Parm.DocId],
-                        HttpUtility.UrlDecode(context.Request.Params[Parm.RelayUrl]),
-                        long.Parse(context.Request.Params[Parm.LogSequenceNumber] ?? "0"));
+                                             context.Request.Params[Parm.DocTypeName],
+                                             context.Request.Params[Parm.DocId],
+                                             HttpUtility.UrlDecode(context.Request.Params[Parm.RelayUrl]),
+                                             long.Parse(context.Request.Params[Parm.LogSequenceNumber] ?? "0"));
                 else
-                    docData = ((BaseDoc)(context.Request.Params.AllKeys.Any(m => m == Parm.DocCache)
+                    docData = ((BaseDoc) (context.Request.Params.AllKeys.Any(m => m == Parm.DocCache)
                                               ? HttpRuntime.Cache[context.Request.Params[Parm.DocCache]]
                                               : Compressor.DecompressFromBase64String(HttpUtility.UrlDecode(context.Request.Params[Parm.DocBin]))
                                                           .FromBytes<BaseDoc>()));
@@ -221,37 +222,34 @@ namespace Rudine.Interpreters
                 DocProcessingInstructions _DocProcessingInstructions;
 
                 //TODO:need to tidy up this code block as its really hard to follow.. In the end, that docData may be a POCO, raw bytes or text... Need to rewrites it's PI here before it's shoved over the repsonse.stream
-                if (docData is BaseDoc)
-                {
+                if (docData is BaseDoc) {
                     // get the interpreter & convert docData from poco to its raw form
-                    _DocBaseInterpreter = InstanceLocatorByName<DocBaseInterpreter>(((BaseDoc)docData).DocTypeName, ((BaseDoc)docData).solutionVersion);
+                    _DocBaseInterpreter = InstanceLocatorByName<DocBaseInterpreter>(((BaseDoc) docData).DocTypeName, ((BaseDoc) docData).solutionVersion);
                     if (_DocBaseInterpreter is DocByteInterpreter)
-                        docData = ((DocByteInterpreter)_DocBaseInterpreter).WriteByte((BaseDoc)docData);
+                        docData = ((DocByteInterpreter) _DocBaseInterpreter).WriteByte((BaseDoc) docData);
                     else
-                        docData = ((DocTextInterpreter)_DocBaseInterpreter).WriteText((BaseDoc)docData);
+                        docData = ((DocTextInterpreter) _DocBaseInterpreter).WriteText((BaseDoc) docData);
                 }
 
-                if (docData is byte[])
-                {
-                    _DocBaseInterpreter = _DocBaseInterpreter ?? LocateInstance((byte[])docData);
-                    _DocProcessingInstructions = Instance.ReadDocPI((byte[])docData);
-                    docData = Instance.ModPI((byte[])docData, href: DocBaseInterpreter.BuildHref(context, _DocProcessingInstructions.DocTypeName, _DocProcessingInstructions.solutionVersion));
-                    context.Response.BinaryWrite((byte[])docData);
+                if (docData is byte[]) {
+                    _DocBaseInterpreter = _DocBaseInterpreter ?? LocateInstance((byte[]) docData);
+                    _DocProcessingInstructions = Instance.ReadDocPI((byte[]) docData);
+                    docData = Instance.ModPI((byte[]) docData, href: DocBaseInterpreter.BuildHref(context, _DocProcessingInstructions.DocTypeName, _DocProcessingInstructions.solutionVersion));
+                    context.Response.BinaryWrite((byte[]) docData);
                 }
-                else
-                {
-                    _DocBaseInterpreter = _DocBaseInterpreter ?? LocateInstance((string)docData);
-                    _DocProcessingInstructions = Instance.ReadDocPI((string)docData);
-                    docData = Instance.ModPI((string)docData, href: DocBaseInterpreter.BuildHref(context, _DocProcessingInstructions.DocTypeName, _DocProcessingInstructions.solutionVersion));
-                    context.Response.Write((string)docData);
+                else {
+                    _DocBaseInterpreter = _DocBaseInterpreter ?? LocateInstance((string) docData);
+                    _DocProcessingInstructions = Instance.ReadDocPI((string) docData);
+                    docData = Instance.ModPI((string) docData, href: DocBaseInterpreter.BuildHref(context, _DocProcessingInstructions.DocTypeName, _DocProcessingInstructions.solutionVersion));
+                    context.Response.Write((string) docData);
                 }
 
                 context.Response.ContentType = _DocBaseInterpreter.ContentInfo.ContentType;
                 context.Response.AddHeader(
-                    "content-disposition",
-                    string.Format(
-                        "attachment; filename=\"{0}\";",
-                        DocBaseInterpreter.GetFilename(_DocProcessingInstructions, context.Request.Params["ContentFileExtension"])));
+                           "content-disposition",
+                           string.Format(
+                               "attachment; filename=\"{0}\";",
+                               DocBaseInterpreter.GetFilename(_DocProcessingInstructions, context.Request.Params["ContentFileExtension"])));
             }
         }
 
@@ -288,7 +286,9 @@ namespace Rudine.Interpreters
                 .ReadDocTypeName(DocData);
 
         public List<ContentInfo> TemplateSources() =>
-            ContentInterpreterInstances.SelectMany(ContentInterpreterInstance => ContentInterpreterInstance.TemplateSources()).Distinct().ToList();
+            ContentInterpreterInstances.SelectMany(ContentInterpreterInstance => ContentInterpreterInstance.TemplateSources())
+                                       .Distinct()
+                                       .ToList();
 
         public void Validate(byte[] DocData) =>
             LocateInstance(DocData)
@@ -298,8 +298,7 @@ namespace Rudine.Interpreters
             LocateInstance(DocData)
                 .Validate(DocData);
 
-        public byte[] WriteByte<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions
-        {
+        public byte[] WriteByte<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions {
             string DocTypeName, DocRev;
             if (!RuntimeTypeNamer.TryParseDocNameAndRev(source.GetType()
                                                               .Namespace, out DocTypeName, out DocRev))
@@ -316,25 +315,22 @@ namespace Rudine.Interpreters
             LocateInstance(DocData)
                 .WritePI(DocData, pi);
 
-        public MemoryStream WriteStream<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions
-        {
+        public MemoryStream WriteStream<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions {
             DocBaseInterpreter _DocBaseInterpreter = InstanceLocatorByName<DocBaseInterpreter>(source.DocTypeName, source.solutionVersion);
             MemoryStream _MemoryStream = new MemoryStream();
 
-            if (_DocBaseInterpreter is DocByteInterpreter)
-            {
-                byte[] Docx = ((DocByteInterpreter)_DocBaseInterpreter).WriteByte(source, includeProcessingInformation);
+            if (_DocBaseInterpreter is DocByteInterpreter) {
+                byte[] Docx = ((DocByteInterpreter) _DocBaseInterpreter).WriteByte(source, includeProcessingInformation);
                 _MemoryStream.Write(Docx, 0, Docx.Length);
                 _MemoryStream.Position = 0;
                 return _MemoryStream;
             }
 
-            return ((DocTextInterpreter)_DocBaseInterpreter).WriteText(source, includeProcessingInformation)
+            return ((DocTextInterpreter) _DocBaseInterpreter).WriteText(source, includeProcessingInformation)
                                                              .AsMemoryStream();
         }
 
-        public string WriteText<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions
-        {
+        public string WriteText<T>(T source, bool includeProcessingInformation = true) where T : DocProcessingInstructions {
             string DocTypeName, DocRev;
             if (!RuntimeTypeNamer.TryParseDocNameAndRev(source.GetType()
                                                               .Namespace, out DocTypeName, out DocRev))
