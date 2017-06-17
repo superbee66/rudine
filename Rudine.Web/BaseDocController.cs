3e898e7d0343a56136f1424c296f29201a048649
g@progablab.com <g@progablab.com>
2017-06-17 13:18:50 -0700

    ReadStream utilizes spork to fork execution paths to bytes or text now

--- Rudine.Web/BaseDocController.cs
+++ Rudine.Web/BaseDocController.cs
@@ -7,10 +7,8 @@ using Rudine.Web.Util;
 
 #endregion
 
-namespace Rudine.Web
-{
-    public abstract class BaseDocController : IBaseDocController, IBaseDocTemplateBuilder
-    {
+namespace Rudine.Web {
+    public abstract class BaseDocController : IBaseDocController, IBaseDocTemplateBuilder {
         public abstract List<LightDoc> Audit(string DocTypeName, string DocId, string RelayUrl = null);
         public abstract BaseDoc Create(BaseDoc Doc, Dictionary<string, string> DocKeys, string RelayUrl = null);
         public abstract DocRev CreateTemplate(List<DocRevEntry> docFiles, string docTypeName = null, string docRev = null, string schemaXml = null, List<CompositeProperty> schemaFields = null);
@@ -19,27 +17,16 @@ namespace Rudine.Web
         public abstract List<ContentInfo> Interpreters();
         public abstract List<LightDoc> List(List<string> DocTypeNames, Dictionary<string, List<string>> DocKeys = null, Dictionary<string, List<string>> DocProperties = null, string KeyWord = null, int PageSize = 150, int PageIndex = 0, string RelayUrl = null);
         public abstract BaseDoc ReadBytes(byte[] DocData, string RelayUrl = null);
-        public BaseDoc ReadStream(Stream DocData, string RelayUrl = null)
-        {
-            using (MemoryStream _MemoryStream = new MemoryStream())
-            {
-                _MemoryStream.Position = 0;
-                DocData.Position = 0;
-                DocData.CopyTo(_MemoryStream);
-                _MemoryStream.Position = 0;
-                using (StreamReader _StreamReader = new StreamReader(_MemoryStream))
-                {
-                    bool isBinary = FileSystem.isBinary(_StreamReader);
-                    _MemoryStream.Position = 0;
-                    return !isBinary
-                               ? ReadText(_StreamReader.ReadToEnd(), RelayUrl)
-                               : ReadBytes(_MemoryStream.ToArray(), RelayUrl);
-                }
-            }
-        }
+
+        public BaseDoc ReadStream(Stream DocData, string RelayUrl = null) =>
+            DocData.Spork(
+                streamAsValue => ReadBytes(streamAsValue, RelayUrl),
+                streamAsValue => ReadText(streamAsValue, RelayUrl));
+
         public abstract BaseDoc ReadText(string DocData, string RelayUrl = null);
         public abstract LightDoc Status(string DocTypeName, Dictionary<string, string> DocKeys, bool DocStatus, string DocSubmittedByEmail, string RelayUrl = null);
         public abstract LightDoc SubmitBytes(byte[] DocData, string DocSubmittedByEmail, string RelayUrl = null, bool? DocStatus = null, DateTime? SubmittedDate = null, Dictionary<string, string> DocKeys = null, string DocTitle = null);
+
         /// <summary>
         /// </summary>
         /// <param name="DocData">detected as bytes or a string</param>
@@ -50,12 +37,11 @@ namespace Rudine.Web
         /// <param name="DocKeys"></param>
         /// <param name="DocTitle"></param>
         /// <returns></returns>
-        public LightDoc SubmitStream(Stream DocData, string DocSubmittedByEmail, string RelayUrl = null, bool? DocStatus = null, DateTime? SubmittedDate = null, Dictionary<string, string> DocKeys = null, string DocTitle = null)
-        {
-            return DocData.Spork(
+        public LightDoc SubmitStream(Stream DocData, string DocSubmittedByEmail, string RelayUrl = null, bool? DocStatus = null, DateTime? SubmittedDate = null, Dictionary<string, string> DocKeys = null, string DocTitle = null) =>
+            DocData.Spork(
                 streamAsValue => SubmitBytes(streamAsValue, DocSubmittedByEmail, RelayUrl, DocStatus, SubmittedDate, DocKeys, DocTitle),
                 streamAsValue => SubmitText(streamAsValue, DocSubmittedByEmail, RelayUrl, DocStatus, SubmittedDate, DocKeys, DocTitle));
-        }
+
         public abstract LightDoc SubmitText(string DocData, string DocSubmittedByEmail, string RelayUrl = null, bool? DocStatus = null, DateTime? SubmittedDate = null, Dictionary<string, string> DocKeys = null, string DocTitle = null);
         public abstract List<ContentInfo> TemplateSources();
     }
