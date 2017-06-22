31dcfe5d9d7f3f3df3e6fcc282cace1fe1945dd0
G <gbruno@progablab.com>
2017-06-21 20:44:39 -0700

    CreateTest added, fails right now :(

--- RudineTests/DocExchangeTests.cs
+++ RudineTests/DocExchangeTests.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Rudine;
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using Rudine.Interpreters.Embeded;
+using Rudine.Template;
 using Rudine.Web;
 using RudineTests.Properties;
 
@@ -37,10 +39,38 @@ namespace Rudine.Tests
 
             Assert.AreEqual(docRev.DocURN.DocTypeName, pi.DocTypeName);
             Assert.NotNull(docRev.solutionVersion);
+            Assert.IsTrue(docRev.DocChecksum != default(int));
 
             LightDoc lightDoc = DocExchange.Instance.SubmitBytes(new EmbededInterpreter().WriteByte(docRev), "removeThisDocSubmittedByEmail@ok.com");
 
             Assert.NotNull(lightDoc);
+        }
+
+        [Test()]
+        [Sequential]
+        public void CreateTest(
+            [Values(nameof(Resources.BaseLineInfoPath2013), nameof(Resources.BaseLineOpenOffice4))] string docTypeName,
+            [Values("xsn", "pdf")] string fileExtension
+        )
+        {
+            CreateTemplateTest(docTypeName, fileExtension);
+
+            Type DocType = Runtime.ActivateBaseDocType(
+                     docTypeName,
+                     TemplateController.Instance.TopDocRev(docTypeName),
+                     DocExchange.Instance);
+
+            Dictionary<string, string> DocKeys = new Dictionary<string, string> {
+                {"RightNow",DateTime.Now.ToString() },
+                {"CreateTestDocTypeName", docTypeName }
+            };
+
+            BaseDoc baseDoc = (BaseDoc)Activator.CreateInstance(DocType);
+            baseDoc.DocKeys = DocKeys;
+
+            BaseDoc createdBaseDoc = DocExchange.Instance.Create(baseDoc);
+
+            Assert.AreEqual(baseDoc.DocKeys, createdBaseDoc.DocKeys);
         }
     }
 }
