9d02a288593ea48db6a79b9337dca7d71241bc9d
g@progablab.com <g@progablab.com>
2017-07-04 21:11:07 -0700

    super class for sequential nuint test property attributes

---
+++ RudineTests/DocDataResourceByteValues.cs
@@ -0,0 +1,15 @@
+﻿using RudineTests.Properties;
+
+namespace Rudine.Tests
+{
+    class DocTypeNameValues : NUnit.Framework.ValuesAttribute
+    {
+        public DocTypeNameValues() : base(nameof(Resources.BaseLineInfoPath2013), nameof(Resources.BaseLineOpenOffice4)) { }
+    }
+
+    class FileExtensionValues : NUnit.Framework.ValuesAttribute
+    {
+        public FileExtensionValues() : base("xsn", "pdf") { }
+    }
+
+}
