420a1a33b467c0c44a5946eb6a7e8f3d1040d958
G <gbruno@progablab.com>
2017-07-06 22:04:06 -0700

    namespace match to file struct

--- RudineTests/ObjectExtensions.cs
+++ RudineTests/ObjectExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-namespace RudineTests
+namespace Rudine.Tests
 {
     public static class ObjectExtensions
     {
