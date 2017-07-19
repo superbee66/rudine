0706d47f2fbc8c56945b9276dfe84cfff055cc83
g@progablab.com <g@progablab.com>
2017-07-18 22:43:54 -0700

    SyncTemplatesTest being setup, not complete

--- RudineTests/ImporterControllerTests.cs
+++ RudineTests/ImporterControllerTests.cs
@@ -1,19 +1,36 @@
 ﻿using NUnit.Framework;
 using Rudine;
+using Rudine.Web;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Rudine.Template.Filesystem;
+using Rudine.Web.Util;
 
 namespace Rudine.Tests
 {
     [TestFixture()]
-    public class ImporterControllerTests
+    public class ImporterControllerTests : DocExchangeTests
     {
+
+
         [Test()]
-        public void TryDocRevImportingTest()
+        public void SyncTemplatesTest()
         {
+            Assert.AreEqual(0, DocExchange.Instance.List(new List<string> { nameof(DocRev) }).Count());
             ImporterController.SyncTemplates(DocExchange.Instance);
+
+            Assert.AreEqual(0, DocExchange.Instance.List(new List<string> { nameof(DocRev) }).Count());
+
+            new DirectoryInfo(FilesystemTemplateController.DirectoryPath).rmdir();
+            var dir = new DirectoryInfo(FilesystemTemplateController.DirectoryPath).mkdir();
+
+            File.WriteAllBytes(dir.FullName + "\\" + nameof(Properties.Resources.BaseLineInfoPath2013) + "." + Properties.Resources.BaseLineInfoPath2013_Extension, Properties.Resources.BaseLineInfoPath2013);
+
+            ImporterController.SyncTemplates(DocExchange.Instance);
+            Assert.AreEqual(1, DocExchange.Instance.List(new List<string> { nameof(DocRev) }).Count());
         }
     }
 }
---
+++ RudineTests/Web/LightDocTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Rudine.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rudine.Web.Tests
+{
+    [TestFixture()]
+    public class LightDocTests
+    {
+        [Test()]
+        public void CompareToTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void FromObjectTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ParseTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
