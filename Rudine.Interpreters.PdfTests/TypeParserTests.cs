caccf5d26dacc17dcc772ed0be4ff60aef6e6256
g@progablab.com <g@progablab.com>
2017-09-06 00:09:50 -0700

    added TypeParser that will be used to translate PDF's number field into a CLR numeric type

---
+++ Rudine.Interpreters.PdfTests/TypeParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rudine.Interpreters.Pdf.Tests {
+    [TestFixture]
+    public class TypeParserTests {
+        [Test]
+        public void LcdTypeTest() {
+            Assert.AreEqual(TypeParser.LcdType(int.MinValue.ToString(), int.MaxValue.ToString()), typeof(Int32));
+            Assert.AreEqual(TypeParser.LcdType(int.MinValue.ToString(), int.MaxValue.ToString()), typeof(int));
+            Assert.AreEqual(TypeParser.LcdType(int.MinValue.ToString(), long.MaxValue.ToString()), typeof(long));
+            Assert.AreEqual(TypeParser.LcdType(long.MinValue.ToString(), int.MaxValue.ToString()), typeof(long));
+            Assert.AreEqual(TypeParser.LcdType(uint.MinValue.ToString(), uint.MaxValue.ToString()), typeof(UInt32));
+        }
+    }
+}
