0cbeb879f10525b88fa7c0b11e2bd0f8e1ea8acd
Ge <git@progablab.com>
2018-06-04 18:55:09 -0700

    Adding interpreters to accept random external documents not generated by this system

---
+++ Rudine.Interpeter.pdf/ExternalPdfInterpreter.cs
@@ -0,0 +1,28 @@
+﻿namespace Rudine.Interpreters.Pdf
+{
+    public class ExternalPdfInterpreter : PdfInterpreter
+    {
+        public static readonly string MyOnlyDocTypeName = nameof(ExternalPdfInterpreter).Replace("Interpreter", string.Empty);
+        public static readonly string MyOnlyDocTypeVersion = "1.0.0.0";
+
+        public override string ReadDocTypeName(byte[] docData)
+        {
+            string _DocTypeName = base.ReadDocTypeName(docData);
+
+            return string.IsNullOrWhiteSpace(_DocTypeName)
+                ? MyOnlyDocTypeName
+                : _DocTypeName;
+        }
+
+        public override string ReadDocRev(byte[] docData)
+        {
+            string _ReadDocRev = base.ReadDocRev(docData);
+            return string.IsNullOrWhiteSpace(_ReadDocRev)
+                ? MyOnlyDocTypeVersion
+                : _ReadDocRev;
+        }
+
+        public override bool Processable(string docTypeName, string docRev) => 
+            docTypeName == MyOnlyDocTypeName && docRev == MyOnlyDocTypeVersion;
+    }
+}
