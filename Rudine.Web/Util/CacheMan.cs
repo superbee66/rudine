b68cd8c10e6cdee25094999b03c1af0283c2d518
Ge <git@progablab.com>
2018-10-02 12:48:10 -0700

    Cache null values also

--- Rudine.Web/Util/CacheMan.cs
+++ Rudine.Web/Util/CacheMan.cs
@@ -2,8 +2,6 @@
 using System.Linq;
 using System.Runtime.Caching;
 
-//TODO:change public visibility of Util classes as they are not what the consuming developer should concentrate on within inteli-sense
-
 namespace Rudine.Web.Util
 {
     internal static class CacheMan
@@ -11,33 +9,30 @@ namespace Rudine.Web.Util
         private static MemoryCache _MemoryCache;
 
         /// <summary>
-        ///     makes a single solid key string taking Array datatypes into consideration by performing recursive calls
+        ///     placeholder for null items to be cached since you can not feed MemoryCache null
         /// </summary>
-        /// <param name="cacheKeyParts"></param>
-        /// <returns></returns>
-        private static string MakeKey(params object[] cacheKeyParts)
+        private static readonly object _MemoryCacheNullEntry = new object();
+
+        static CacheMan()
         {
-            return string.Join(
-                "__",
-                cacheKeyParts.Select(o =>
-                                         string.Format("{0}",
-                                             o.GetType().IsArray && ((object[]) o).Length > 0
-                                                 ? MakeKey(((Array) o).Cast<object>())
-                                                 : o)));
+            Clear();
         }
 
-        static CacheMan() { Clear(); }
-
-        public static T Cache<T>(Func<T> itemFactory, bool forcedRefresh = false, params object[] cacheKeyParts) where T : class
-        {
-            string key = MakeKey(cacheKeyParts);
-            return Cache(
+        /// <summary>
+        ///     grabs items from MemoryCache if they are available
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="itemFactory">>the factory to run if the item can't be found in cache</param>
+        /// <param name="forcedRefresh">runs the itemFactory regardless</param>
+        /// <param name="cacheKeyParts">things that will have there ToString() called then concatenated togather to make a key</param>
+        /// <returns></returns>
+        public static T Cache<T>(Func<T> itemFactory, bool forcedRefresh = false, params object[] cacheKeyParts) where T : class =>
+            Cache(
                 itemFactory,
                 forcedRefresh,
                 cacheKeyParts != null && cacheKeyParts.Length > 0
-                    ? key
+                    ? MakeKey(cacheKeyParts)
                     : null);
-        }
 
         /// <summary>
         ///     grabs items from MemoryCache if they are available
@@ -45,7 +40,7 @@ namespace Rudine.Web.Util
         /// <typeparam name="T"></typeparam>
         /// <param name="itemFactory">the factory to run if the item can't be found in cache</param>
         /// <param name="forcedRefresh">runs the itemFactory regardless</param>
-        /// <param name="cacheKey"></param>
+        /// <param name="cacheKey">default is the type name of the itemFactory passed</param>
         /// <returns></returns>
         public static T Cache<T>(Func<T> itemFactory, bool forcedRefresh = false, string cacheKey = null) where T : class
         {
@@ -53,21 +48,31 @@ namespace Rudine.Web.Util
                 cacheKey = string.Format("{0}", typeof(T));
 
             if (!_MemoryCache.Contains(cacheKey) || forcedRefresh)
-            {
-                //TODO:Potential performance issure with conditionally caching values only if there not null
-                T o = itemFactory.Invoke();
-                if (o == null)
-                    return null;
-                _MemoryCache[cacheKey] = o;
-            }
-            return (T) _MemoryCache[cacheKey];
+                _MemoryCache[cacheKey] = itemFactory.Invoke() ?? _MemoryCacheNullEntry;
+
+            return _MemoryCache[cacheKey] == _MemoryCacheNullEntry
+                ? null
+                : (T) _MemoryCache[cacheKey];
         }
 
         public static void Clear()
         {
-            if (_MemoryCache != null)
-                _MemoryCache.Dispose();
+            _MemoryCache?.Dispose();
             _MemoryCache = new MemoryCache(typeof(CacheMan).Name);
         }
+
+        /// <summary>
+        ///     makes a single solid key string taking Array datatypes into consideration by performing recursive calls
+        /// </summary>
+        /// <param name="cacheKeyParts"></param>
+        /// <returns></returns>
+        private static string MakeKey(params object[] cacheKeyParts) =>
+            string.Join(
+                "__",
+                cacheKeyParts.Select(o =>
+                    string.Format("{0}",
+                        o.GetType().IsArray && ((object[]) o).Length > 0
+                            ? MakeKey(((Array) o).Cast<object>())
+                            : o)));
     }
 }
